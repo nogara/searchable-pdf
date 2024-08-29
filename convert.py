3b8d6b855ac5d2a807a6285f4812b15431256080
Luiz Gustavo Nogara <nogara@gmail.com>
2024-08-29 12:20:11 -0300

    Log some of the processing steps

--- convert.py
+++ convert.py
@@ -20,6 +20,11 @@ input_folder = "input"
 # define the output folder
 output_folder = "output"
 
+def log(message):
+    # get the current time
+    current_time = pd.Timestamp.now().strftime("%Y-%m-%d %H:%M:%S")
+    # print the message with the current time
+    print(f"{current_time} - {message}")
 
 # get a list of the files already present in the output folder that match your input folder
 # skip these files later in the code
@@ -34,17 +39,25 @@ for pdf_file in glob.glob(os.path.join(input_folder, "*.pdf")):
 
     # check if the output file already exists and needs to be skipped
     if base_filename in existing_output_files:
-        print(f"Skipping '{base_filename}' as it already exisits in the output folder.")
+        log(f"Skipping '{base_filename}' as it already exisits in the output folder.")
         continue  # skip through to the next code block
 
+    log(f"Processing '{base_filename}'.")
 
+    log("Converting PDF to images.")
     # convert PDF to image
     images = convert_from_path(pdf_file)
     # create the PDF writer object
     pdf_writer = PyPDF2.PdfWriter()
 
+    # get number of images
+    num_images = len(images)
+    log(f"Processing {num_images} images")
+
     # Loop to append images together as pages in a document
     for image in images:
+        # print the current image number
+        print(f"Processing image {images.index(image) + 1} of {num_images}")
         page = pytesseract.image_to_pdf_or_hocr(image, extension="pdf")
         pdf = PyPDF2.PdfReader(io.BytesIO(page))
         pdf_writer.add_page(pdf.pages[0])
@@ -56,4 +69,4 @@ for pdf_file in glob.glob(os.path.join(input_folder, "*.pdf")):
     with open(output_file, "wb") as f:
         pdf_writer.write(f)
 
-    print(f"'{base_filename}' processed and saved to '{output_file}'.")
+    log(f"'{base_filename}' processed and saved to '{output_file}'.")
