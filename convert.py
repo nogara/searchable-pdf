a544821e69b7fb30faa3218d0a767a195e61cbb3
Luiz Gustavo Nogara <nogara@gmail.com>
2024-08-29 12:19:39 -0300

    Use poppler and tesseract from PATH

--- convert.py
+++ convert.py
@@ -14,14 +14,6 @@ from pdf2image import convert_from_path
 # change to black and white with Pillow to take up less space
 from PIL import Image
 
-# define paths
-
-# poppler_path will be mapped to a libaray/bin
-poppler_path = "/opt/homebrew/Cellar/poppler/24.04.0_1/bin"
-
-# pytesseract will be mapped to an .exe
-pytesseract.pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"
-
 # define the input folder
 input_folder = "input"
 
@@ -45,9 +37,9 @@ for pdf_file in glob.glob(os.path.join(input_folder, "*.pdf")):
         print(f"Skipping '{base_filename}' as it already exisits in the output folder.")
         continue  # skip through to the next code block
 
-    # convert PDF to image
-    images = convert_from_path(pdf_file, poppler_path=poppler_path)
 
+    # convert PDF to image
+    images = convert_from_path(pdf_file)
     # create the PDF writer object
     pdf_writer = PyPDF2.PdfWriter()
 
